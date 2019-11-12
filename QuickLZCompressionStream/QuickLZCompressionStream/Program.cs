5a93f6e389b39b629463671162b1efe9cd50c404
Carolina <carolinaolivares526@gmail.com>
2019-11-11 22:58:41 -0300

    Create Program.cs

---
+++ QuickLZCompressionStream/QuickLZCompressionStream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickLZCompressionStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
